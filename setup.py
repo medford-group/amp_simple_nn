a430fd042ff0355b35e88e75a64302b78766e940
Ben Comer <ben.comer@gatech.edu>
2019-09-06 13:54:04 -0400

    added init

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='amp_simple_nn',
       author='Ben Comer',
       author_email='ben.comer@gatech.edu',
       url='https://github.com/medford-group/amp_simple_nn',
-      scripts=['lammps_interface/bin/py_wrapped_packmol.py'],
       packages=find_packages(),
       install_requires=['spglib', 'numpy>=1.16.0','ase'],
      )
