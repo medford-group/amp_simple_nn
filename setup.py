b4f57b702ed0dc5762bf4d14b71efb1b0d42b94c
Ben Comer <ben.comer@gatech.edu>
2019-09-06 12:56:54 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='amp_simple_nn',
+      version='0.1',
+      description='',
+      author='Ben Comer',
+      author_email='ben.comer@gatech.edu',
+      url='https://github.com/medford-group/amp_simple_nn',
+      scripts=['lammps_interface/bin/py_wrapped_packmol.py'],
+      packages=find_packages(),
+      install_requires=['spglib', 'numpy>=1.16.0','ase', 
+                        'simple_nn'],
+     )
+
+
